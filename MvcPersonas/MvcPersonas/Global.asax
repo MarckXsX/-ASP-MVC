78cde52ea3d0c05f6c827cc720ef24be1a489588
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2023-11-07 15:32:17 -0600

    Ingreso practica

---
+++ MvcPersonas/MvcPersonas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcPersonas.MvcApplication" Language="C#" %>
---
+++ MvcPersonas/MvcPersonas/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcPersonas.Models
+{
+    public class Persona
+    {
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+    public class PersonaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
